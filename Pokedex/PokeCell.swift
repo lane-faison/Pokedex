e4c21c1c35faf6220c0762c6edfc764d2e7f158c
Lane Faison <lane.faison@icloud.com>
2017-05-19 16:06:52 -0600

    Poke images appearing in collection view

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,28 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Lane Faison on 5/19/17.
+//  Copyright © 2017 Lane Faison. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    // When creating cells, create a view like this that attaches to the cell's data
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
