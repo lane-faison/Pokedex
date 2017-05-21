ba326de93e0a43062ae0f27aa960c810a22753e4
Lane Faison <lane.faison@icloud.com>
2017-05-20 18:19:30 -0600

    Details VC labels linked

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -10,12 +10,30 @@ import UIKit
 
 class PokemonDetailVC: UIViewController {
 
-    
     var pokemon: Pokemon!
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var attackLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var evoLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        nameLabel.text = pokemon.name
+        
+        
     }
 
-
+    @IBAction func backBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+   
 }
